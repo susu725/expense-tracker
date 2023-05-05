a7d215c16e85f1e2945199db3b7f05e4fd9efcbe
susu725 <zxcv1010017@gmail.com>
2023-05-05 20:05:17 +0800

    feat: add mongoose & models

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+    console.log(`連線失敗`)
+})
+db.once('open', () => {
+    console.log(`連線成功`)
+})
+
 app.get('/', (req, res) => {
     res.send(`HI`)
 })
