0c66532cc796608e7b84e61fdd9d0ba7d7d17828
susu725 <zxcv1010017@gmail.com>
2023-05-06 21:38:58 +0800

    feat: modify index page ui

--- app.js
+++ app.js
@@ -17,63 +17,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
+app.use(express.static('public'))
+
 const routes = require('./routes')
 app.use(routes)
-
-// // 首頁
-// app.get('/', (req, res) => {
-//     Record.find()
-//         .lean()
-//         .sort({ date: 'asc' })
-//         .then(records => {
-//             records.map(function (record, i) {
-//                 return records[i].date = formatDate(record.date)
-//             })
-//             res.render('index', { records })
-//         })
-//         .catch(err => console.log(err))
-// })
-
-// // 新增
-// app.get('/records/new', (req, res) => {
-//     res.render('new')
-// })
-// app.post('/records', (req, res) => {
-//     const { name, amount } = req.body
-//     Record.create({ name, amount })
-//         .then(() => res.redirect('/'))
-//         .catch(err => console.log(err))
-// })
-
-// // 修改特定資料
-// app.get('/records/:id/edit', (req, res) => {
-//     const id = req.params.id
-//     Record.findById(id)
-//         .lean()
-//         .then(record => res.render('edit', { record }))
-//         .catch(err => console.log(err))
-// })
-// app.put('/records/:id', (req, res) => {
-//     const id = req.params.id
-//     const { name, amount } = req.body
-//     Record.findById(id)
-//         .then(record => {
-//             record.name = name
-//             record.amount = amount
-//             record.save()
-//         })
-//         .then(() => res.redirect('/'))
-//         .catch(err => console.log(err))
-// })
-
-// // 刪除
-// app.delete('/records/:id', (req, res) => {
-//     const id = req.params.id
-//     Record.findById(id)
-//         .then(record => record.remove())
-//         .then(() => res.redirect('/'))
-//         .catch(err => console.log(err))
-// })
 
 app.listen(port, () => {
     console.log(`成功`)
