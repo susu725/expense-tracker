7bc27ab264c96144b32dbe988e02e46d428c674f
susu725 <zxcv1010017@gmail.com>
2023-05-07 20:03:11 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+// 登入
 router.get('/login', (req, res) => {
     res.render('login')
+})
+router.post('/login', (req, res) => {
+
+})
+
+// 註冊
+router.get('/register', (req, res) => {
+    res.render('register')
 })
 
 module.exports = router
