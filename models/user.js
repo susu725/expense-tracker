4e25fdeb523c72c513996f1babfe8669369094a4
susu725 <zxcv1010017@gmail.com>
2023-05-07 20:23:59 +0800

    feat: add registration feature & modify user model

--- models/user.js
+++ models/user.js
@@ -5,6 +5,18 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 module.exports = mongoose.model('User', userSchema)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 // 登入
 router.get('/login', (req, res) => {
     res.render('login')
@@ -12,6 +14,20 @@ router.post('/login', (req, res) => {
 // 註冊
 router.get('/register', (req, res) => {
     res.render('register')
+})
+router.post('/register', (req, res) => {
+    const { name, email, password, confirmPassword } = req.body
+    User.findOne({ email }).then(user => {
+        if (user) {
+            console.log('使用者已經存在!')
+            res.render('register', { name, email, password, confirmPassword })
+        } else {
+            User.create({ name, email, password })
+                .then(() => res.redirect('/'))
+                .catch(err => console.log(err))
+        }
+    })
+        .catch(err => console.log(err))
 })
 
 module.exports = router
