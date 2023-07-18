1d7b1c3ce0d84a44a638f9a874db4a2ae3bd597c
susu725 <zxcv1010017@gmail.com>
2023-07-18 20:10:57 +0800

    feat: add mongoose & dotenv

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
-const app = express()
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+//導入routes
+require('./config/mongoose')
+
+const app = express()
 const PORT = 3000
 
+// 使用routes
 app.get('/', (req, res) => {
     res.send(`HI`)
 })
 
 app.listen(PORT, () => {
-    console.log(`成功`)
+    console.log(`連線成功`)
 })
