2976f029dfc39878a75469fd24a7cfd14c701b9a
susu725 <zxcv1010017@gmail.com>
2023-07-22 23:10:46 +0800

    feat: add register function

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 router.get('/login', (req, res) => {
     res.render('login')
 })
 
 router.get('/register', (req, res) => {
     res.render('register')
+})
+
+router.post('/register', (req, res) => {
+    const { name, email, password, confirmPassword } = req.body
+    if (password !== confirmPassword) {
+        console.log('確認密碼不符')
+    } else {
+        User.findOne({ email }).then(user => {
+            if (user) {
+                res.render('register', { name, email, password, confirmPassword })
+                console.log("用戶已存在")
+            }
+            return User.create({ name, email, password })
+        })
+            .then(() => res.redirect('/'))
+            .catch(err => console.log(err))
+    }
 })
 
 module.exports = router
