ceec9ace2f8d9894a99e8f8e620d19f9530c54c6
susu725 <zxcv1010017@gmail.com>
2023-07-23 00:47:53 +0800

    feat: add authenticator in record and home routers

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const home = require('./modules/home')
 const records = require('./modules/records')
 const users = require('./modules/users')
+const home = require('./modules/home')
 
-router.use('/', home)
-router.use('/records', records)
+const { authenticator } = require('../middleware/auth')
+
+router.use('/records', authenticator, records)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
