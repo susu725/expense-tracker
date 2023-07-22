76a3d15388cb1c1b0159cd8847aab20b77b1d327
susu725 <zxcv1010017@gmail.com>
2023-07-23 04:35:55 +0800

    feat: add facebook login

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const router = express.Router()
 
 const records = require('./modules/records')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 const home = require('./modules/home')
 
 const { authenticator } = require('../middleware/auth')
 
 router.use('/records', authenticator, records)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
