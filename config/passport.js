7cc7738c4de81cf5b088f09ca6c6569b7c795cce
susu725 <zxcv1010017@gmail.com>
2023-07-23 00:31:58 +0800

    feat: add login function and session & passport

---
+++ config/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+const User = require('../models/user')
+
+module.exports = app => {
+    // 初始化Passport模組
+    app.use(passport.initialize())
+    app.use(passport.session())
+
+    // 設定本地登入策略
+    passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+        User.findOne({ email }).then(user => {
+            if (!user) {
+                console.log('用戶不存在')
+                return done(null, false, { message: '用戶不存在' })
+            }
+            if (password !== user.password) {
+                console.log('密碼錯誤')
+                return done(null, false, { message: '密碼錯誤' })
+            }
+            return done(null, user)
+        })
+            .catch(err => done(err, null))
+    }))
+
+    // 設定序列化與反序列化
+    passport.serializeUser((user, done) => {
+        done(null, user.id)
+    })
+
+    passport.deserializeUser((id, done) => {
+        User.findById(id)
+            .lean()
+            .then(user => done(null, user))
+            .catch(err => done(err, null))
+    })
+}
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
 
 const User = require('../../models/user')
 
+// 登入
 router.get('/login', (req, res) => {
     res.render('login')
 })
 
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+}))
+
+// 註冊
 router.get('/register', (req, res) => {
     res.render('register')
 })
@@ -15,17 +23,17 @@ router.post('/register', (req, res) => {
     const { name, email, password, confirmPassword } = req.body
     if (password !== confirmPassword) {
         console.log('確認密碼不符')
-    } else {
-        User.findOne({ email }).then(user => {
-            if (user) {
-                res.render('register', { name, email, password, confirmPassword })
-                console.log("用戶已存在")
-            }
-            return User.create({ name, email, password })
-        })
-            .then(() => res.redirect('/'))
-            .catch(err => console.log(err))
+        return res.render('register', { name, email, password, confirmPassword })
     }
+    return User.findOne({ email }).then(user => {
+        if (user) {
+            console.log("用戶已存在")
+            return res.render('register', { name, email, password, confirmPassword })
+        }
+        return User.create({ name, email, password })
+            .then(() => res.redirect('/'))
+    })
+        .catch(err => console.log(err))
 })
 
 module.exports = router
